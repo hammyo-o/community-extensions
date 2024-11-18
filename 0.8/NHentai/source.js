2d54203e6ae12839a924a68c8d2e78dc6717464d
pisshammy <165633434+hammyo-o@users.noreply.github.com>
2024-11-17 21:40:24 -0600

    rread

--- 0.8/NHentai/source.js
+++ 0.8/NHentai/source.js
@@ -1253,7 +1253,7 @@ var _Sources = (() => {
   // src/NHentai/NHentai.ts
   var NHENTAI_URL = "https://nhentai.net";
   var NHentaiInfo = {
-    version: "4.0.8",
+    version: "4.0.81",
     name: "nhentai",
     icon: "icon.png",
     author: "NotMarek & Netsky",
